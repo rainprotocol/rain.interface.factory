80930d8c10e254d496994cbd7ecf0cb29c34f849
thedavidmeister <thedavidmeister@gmail.com>
2023-03-28 21:34:19 +0400

    docs

--- src/ICloneFactoryV1.sol
+++ src/ICloneFactoryV1.sol
@@ -1,11 +1,26 @@
 // SPDX-License-Identifier: CAL
 pragma solidity ^0.8.18;
 
+/// @title ICloneableFactoryV1
+/// @notice A minimal interface to create proxy clones of a reference bytecode
+/// and emit events so that indexers can discover it. `ICloneableFactoryV1` knows
+/// nothing about the contracts that it clones, instead relying only on the
+/// minimal `ICloneableV1` interface being implemented on the reference bytecode.
 interface ICloneableFactoryV1 {
+    /// Emitted upon each `clone`.
+    /// @param sender The `msg.sender` that called `clone`.
+    /// @param implementation The reference bytecode to clone as a proxy.
+    /// @param clone The address of the new proxy contract.
     event NewClone(address sender, address implementation, address clone);
 
-    /// Creates a new child contract.
-    ///
+    /// Clones an implementation using a proxy. EIP1167 proxy as used by Open
+    /// Zeppelin is recommended but the exact cloning procedure is not specified
+    /// by this interface. The factory MUST call `ICloneableV1.initialize`
+    /// atomically with the cloning process and MUST NOT call any other functions
+    /// on the cloned proxy before initialize completes successfully. If the
+    /// initialize reverts then the `clone` MUST revert.
+    /// MUST emit `NewClone` with the implementation and clone address.
+    /// @param implementation The contract to clone.
     /// @param data As per `ICloneableV1`.
     /// @return New child contract address.
     function clone(address implementation, bytes calldata data) external returns (address);
--- src/ICloneableV1.sol
+++ src/ICloneableV1.sol
@@ -3,13 +3,24 @@ pragma solidity ^0.8.18;
 
 /// @title ICloneableV1
 /// @notice Minimal interface following the Open Zeppelin conventions for
-/// initializing a cloned proxy. To be fully generic the initilize accepts
-/// `bytes` and so MUST abi decode within the initialize function. This allows
-/// the factory to service arbitrary cloneable proxies but also erases the type
-/// of the initialization config from the ABI. One workaround is to emit an event
-/// containing the initialization config type, so that the type appears within
-/// the event and therefore the ABI.
+/// initializing a cloned proxy.
 interface ICloneableV1 {
-    /// Initialize is intended to work
+    /// Initialize is intended to work like constructors but for cloneable
+    /// proxies. The `ICloneableV1` contract MUST ensure that initialize cannot
+    /// be called more than once. The `ICloneableV1` contract is designed to be
+    /// deployed by an `ICloneFactoryV1` but MUST NOT assume that it will be. It
+    /// is possible for someone to directly deploy an `ICloneableV1` and fail to
+    /// call initialize before other functions are called, and end users MAY NOT
+    /// realise or know how to confirm a safe deployment state. The
+    /// `ICloneableV1` MUST take appropriate measures to ensure that functions
+    /// called before initialize are safe to do so, or revert.
+    ///
+    /// To be fully generic `initilize` accepts `bytes` and so MUST ABI decode
+    /// within the initialize function. This allows the factory to service
+    /// arbitrary cloneable proxies but also erases the type of the
+    /// initialization config from the ABI. One workaround is to emit an event
+    /// containing the initialization config type, so that the type appears
+    /// within the event and therefore the ABI.
+    /// @param data The initialization data.
     function initialize(bytes calldata data) external;
 }
