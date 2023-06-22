113cd5457997502cb56bd7540cbc7d8b6870940c
thedavidmeister <thedavidmeister@gmail.com>
2023-06-22 17:14:41 +0400

    fmt

--- src/CloneFactory.sol
+++ src/CloneFactory.sol
@@ -9,20 +9,15 @@ import {Clones} from "openzeppelin-contracts/contracts/proxy/Clones.sol";
 /// Thrown when an implementation is the zero address which is always a mistake.
 error ZeroImplementation();
 
-bytes32 constant CLONE_FACTORY_META_HASH = bytes32(
-    0xae0fb5b68fe1791c72509bf46ea6abf6a982d21451265be0a017f7959712a67e
-);
+bytes32 constant CLONE_FACTORY_META_HASH = bytes32(0xae0fb5b68fe1791c72509bf46ea6abf6a982d21451265be0a017f7959712a67e);
 
 contract CloneFactory is ICloneableFactoryV1, DeployerDiscoverableMetaV1 {
-    constructor(
-        DeployerDiscoverableMetaV1ConstructionConfig memory config_
-    ) DeployerDiscoverableMetaV1(CLONE_FACTORY_META_HASH, config_) {}
+    constructor(DeployerDiscoverableMetaV1ConstructionConfig memory config_)
+        DeployerDiscoverableMetaV1(CLONE_FACTORY_META_HASH, config_)
+    {}
 
     /// @inheritdoc ICloneableFactoryV1
-    function clone(
-        address implementation_,
-        bytes calldata data_
-    ) external returns (address) {
+    function clone(address implementation_, bytes calldata data_) external returns (address) {
         if (implementation_ == address(0)) {
             revert ZeroImplementation();
         }
