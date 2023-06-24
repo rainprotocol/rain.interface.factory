8629b9aa84c977dc612471ed3ec700dd9e795b2a
thedavidmeister <thedavidmeister@gmail.com>
2023-06-25 01:47:05 +0400

    debug deploy

--- script/DeployCloneFactory.sol
+++ script/DeployCloneFactory.sol
@@ -17,6 +17,9 @@ contract DeployCloneFactory is Script {
         // hardcoded from CI https://github.com/rainprotocol/rain-protocol/actions/runs/5039345251/jobs/9037426821
         address i9rDeployer = 0xB20DFEdC1b12AA6afA308064998A28531a18C714;
 
+        console2.log("meta hash:");
+        console2.logBytes32(keccak256(meta));
+
         vm.startBroadcast(deployerPrivateKey);
         CloneFactory cloneFactory = new CloneFactory(DeployerDiscoverableMetaV1ConstructionConfig (
             i9rDeployer,
