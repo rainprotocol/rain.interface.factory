e4b694b3b94739eb97eaf299a47964d757f84ab6
thedavidmeister <thedavidmeister@gmail.com>
2023-06-28 16:39:30 +0400

    fix tests

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
       in rec {
         packages = rec {
           build-meta = pkgs.writeShellScriptBin "build-meta" ''
-            forge build && \
+            forge build --force && \
             ${rain-cli} meta build -o meta/CloneFactory.rain.meta -i <(${rain-cli} meta solc artifact -c abi -i out/CloneFactory.sol/CloneFactory.json) -m solidity-abi-v2 -t json -e deflate -l en
           '';
 
